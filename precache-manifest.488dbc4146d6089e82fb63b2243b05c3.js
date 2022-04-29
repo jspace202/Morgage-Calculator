3d086d0cacf017ee8b43ec7d1b1f5c0e37c04ba3
Jaden Patel <jspace202@gmail.com>
2022-04-28 23:23:37 -0500

    Updates

---
+++ precache-manifest.488dbc4146d6089e82fb63b2243b05c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "93645bf325a345b3ad08",
+    "url": "/Morgage-Calculator/static/css/main.8c5a224a.chunk.css"
+  },
+  {
+    "revision": "93645bf325a345b3ad08",
+    "url": "/Morgage-Calculator/static/js/main.93645bf3.chunk.js"
+  },
+  {
+    "revision": "ccfaed377a9312a06ca7",
+    "url": "/Morgage-Calculator/static/js/1.ccfaed37.chunk.js"
+  },
+  {
+    "revision": "4065e7ac02f19078cc29",
+    "url": "/Morgage-Calculator/static/js/runtime~main.4065e7ac.js"
+  },
+  {
+    "revision": "4404ce213a4053ecc0411c4689b656d7",
+    "url": "/Morgage-Calculator/index.html"
+  }
+];
